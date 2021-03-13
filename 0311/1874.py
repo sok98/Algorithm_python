885f34b89ee985ab51a4f1a137b89b86ee7f7919
yeseul <sok98@naver.com>
2021-03-13 22:00:22 +0900

    0313 baekjoon

--- 0311/1874.py
+++ 0311/1874.py
@@ -13,8 +13,8 @@ def s_pop(s):
     result.append("-")
     global k
     k += 1
-    print("testtesttest: ", numbers[k], s[-1])
-    if numbers[k] == s[-1]:
+   # print("testtesttest: ", numbers[k], s[-1])
+    if n > k and numbers[k] == s[-1]:
         s_pop(s)
 
 
