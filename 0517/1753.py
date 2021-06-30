fac801a58fb5f1e4f0c0175992bc86196f524fd0
yeseul <sok98@naver.com>
2021-06-30 20:07:30 +0900

    0629

--- 0517/1753.py
+++ 0517/1753.py
@@ -4,7 +4,3 @@ input = sys.stdin.readline
 V, E = map(int, input().split())
 K = input()
 uvw = [input().split() for _ in range(E)]
-# for _ in range(E):
-#     uvw.append(input().split())
-
-print(uvw)
