796dcad0cd69379be299009fa08a92b990b17cd0
SeoYeseul <sok98@naver.com>
2021-07-20 18:40:43 +0900

    ADD 0720

--- 0720/다리를 지나는 트럭.py
+++ 0720/다리를 지나는 트럭.py
@@ -4,16 +4,19 @@ def solution(bridge_length, weight, truck_weights):
     
     if sum(truck_weights) <= weight:
         return bridge_length + len(truck_weights)
-
+    
+    truck_weights = deque(truck_weights)
     on_bridge = deque([0]*bridge_length)
     cnt_time = 0
-    i = 0
-    while i < len(truck_weights):
+    sum_weight = 0
+
+    while truck_weights:
         cnt_time += 1
-        on_bridge.popleft()
-        if sum(on_bridge) + truck_weights[i] < weight:
-            on_bridge.append(truck_weights[i])
-            i += 1
+        sum_weight -= on_bridge.popleft()
+        if sum(on_bridge) + truck_weights[0] < weight:
+            pop = truck_weights.popleft()
+            on_bridge.append(pop)
+            sum_weight += pop
         else:
             on_bridge.append(0)
 
