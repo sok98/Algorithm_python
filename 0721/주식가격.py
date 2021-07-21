a2e0767a6971ee1aa066171ef1bd927c05ae48e5
SeoYeseul <sok98@naver.com>
2021-07-21 19:04:04 +0900

    ADD 0721

---
+++ 0721/주식가격.py
@@ -0,0 +1,14 @@
+def solution(prices):
+    answer = [i for i in range(len(prices)-1,-1,-1)]
+    print(answer)
+    
+    for before in range(len(prices)):
+        for after in range(before+1,len(prices)):
+            if prices[after] < prices[before]:
+                answer[before] = after-before
+                break
+    
+    return answer
+
+
+print(solution([1,2,3,2,3,1]))  #[5,4,1,2,1,0]
