4e1170ecc8db6d4667b0b3b5671c286a98846868
SeoYeseul <sok98@naver.com>
2021-07-22 10:25:59 +0900

    test

--- test.py
+++ test.py
@@ -1,28 +1,15 @@
-import sys
-input = sys.stdin.readline()
+def solution(numbers):
 
-# N = int(input())
-N = 6
-step = [0]*(N+1)
-count = [0]*(N+1)
+    num_str = [str(num) for num in numbers]
 
-count[0] = 0
-sum = 0
+    num_str.sort(key = lambda x: (x*4)[:4], reverse = True)
+    print(num_str)
 
-for i in range(1, 3):  # 1, 2 계단은 무조건 밟음
-    step[i] = int(input())
-    step[i] += sum
-    sum = step[i]
-    count[i] += 1
-
-
-for i in range(3, N+1):
-    value = int(input())
-
-    if count[i-1] >= 2 or (step[i-1] < step[i-2]):
-        step[i] = step[i-2]+value
+    if num_str[0] != 0:
+        answer = "".join(num_str)
+        return answer
     else:
-        step[i] = step[i-1]+value
-        count[i] = count[i-1]+1
+        return "0"
 
-print(step[N])
+
+print(solution([3, 30, 34, 5, 53, 9]))
