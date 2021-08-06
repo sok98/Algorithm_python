4309f4f5f344a4391961c45b6a53d9b68e7b3be2
SeoYeseul <sok98@naver.com>
2021-08-07 00:59:53 +0900

    ADD 0806

--- 0806/1244.py
+++ 0806/1244.py
@@ -1,43 +1,28 @@
-from collections import deque
+import sys
+input = sys.stdin.readline
+
+change = {1:0, 0:1}
 
 n = int(input())
-switch = deque(map(int, input().split(" ")))
-switch.append(0)
-switch.rotate(1)
-
-def change(n) :
-    if switch[n] == 0:
-        switch[n] = 1
-    else:
-        switch[n] = 0
-
+switch = list(map(int, input().split()))
 s = int(input())
-for _ in range(s):
-    gender, num = map(int, input().split(" "))
 
-    if gender == 1: # 남학생
-        i = 1
-        target = num * i
-        while target < len(switch):
-            change(target)
-            i += 1
-            target = num * i
+for _ in range(s):
+    g, num = map(int, input().split())
+    if g == 1: # 남학생
+        target = num-1
+        while target < n:
+            switch[target] = change[switch[target]]
+            target += num
 
     else:   # 여학생
-        change(num)
+        num -= 1
+        switch[num] = change[switch[num]]
         i = 1
-        while num-i >= 0 and num+i < len(switch) and switch[num-i] == switch[num+i]:
-            change(num-i)
-            change(num+i)
+        while num-i >= 0 and num+i < n and switch[num-i] == switch[num+i]:
+            switch[num-i] = change[switch[num-i]]
+            switch[num+i] = change[switch[num+i]]
             i += 1
 
-switch.popleft()
-if len(switch) > 20:
-    i = 0
-    while i+20 < len(switch):
-        for i in switch[i, i+20]:
-            print(i, end=" ")
-        i += 1
-else:
-    for i in switch:
-        print(i, end=" ")
+for i in range(0, n, 20):
+    print(*switch[i:i+20])
