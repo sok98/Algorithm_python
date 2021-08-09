6be1821d63ec0a65a1bf00259f8263804458e284
SeoYeseul <sok98@naver.com>
2021-08-10 00:30:29 +0900

    ADD 0809

--- 0809/1107.py
+++ 0809/1107.py
@@ -12,14 +12,6 @@ if 100 == N:
 
 else : 
     for count in range(100001):
-        ok = True
-        for i in str(N+count):
-            if i in button:
-                ok = False
-                break
-        if ok == True:
-            print(min(answer, count + len(str(N+count))))
-            break
         
         if N-count >= 0:
             ok = True
@@ -31,3 +23,40 @@ else :
                 print(min(answer, count + len(str(N-count))))
                 break
         
+        ok = True
+        for i in str(N+count):
+            if i in button:
+                ok = False
+                break
+        if ok == True:
+            print(min(answer, count + len(str(N+count))))
+            break
+
+
+
+
+
+# ----- 정답 코드 -----
+# if __name__ == "__main__":
+
+#     enable = {str(i) for i in range(10)}  # 0, 1, 2 ... 9 (가능한 수)
+
+#     # input
+#     N = int(input())  # 이동하려고 하는 채널
+#     M = int(input())  # 고장난 버튼의 개수
+#     if M != 0:
+#         enable -= set(map(str, input().split()))  # 고장난 버튼 리스트 제거
+
+#     # case1 (100번에서 +, - 로만 움직이는 경우)
+#     min_cnt = abs(100 - N)
+
+#     # case2 (1,000,000 채널까지 브루트 포스 진행)
+#     # 500,000 채널까지 존재하기 때문에 500,000 보다 크면서 모든 숫자의 경우를 거치는 1,000,000까지를 범위로 잡음
+#     for num in range(1000001):
+#         num = str(num)
+#         for j in range(len(num)):
+#             if num[j] not in enable:
+#                 break
+#             elif j == len(num) - 1:
+#                 min_cnt = min(min_cnt, abs(N - int(num)) + len(str(num)))
+#     print(min_cnt)
