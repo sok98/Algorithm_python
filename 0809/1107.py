c116c42ca759eabad030c0f223bd7987dc02bfbd
SeoYeseul <sok98@naver.com>
2021-08-10 16:12:59 +0900

    UPDATE 0809

--- 0809/1107.py
+++ 0809/1107.py
@@ -1,18 +1,23 @@
 import sys
 input = sys.stdin.readline
 
+button = []
+
 N = int(input())
 M = int(input())
-button = list(input().split())
+if M != 0:
+    button.extend(list(map(str, input().split())))
 
 answer = abs(N-100)    # +,-로만 이동 횟수
 
-if 100 == N:
+if 100 == N:    # 이동하려는 채널이 현재 있는 채널인 경우
     print(0)
+    
+elif M == 10: # 모든 숫자 버튼이 고장난 경우 : +,-로만 이동
+    print(answer)
 
 else : 
-    for count in range(100001):
-        
+    for count in range(1000001):
         if N-count >= 0:
             ok = True
             for i in str(N-count):
@@ -31,32 +36,3 @@ else :
         if ok == True:
             print(min(answer, count + len(str(N+count))))
             break
-
-
-
-
-
-# ----- 정답 코드 -----
-# if __name__ == "__main__":
-
-#     enable = {str(i) for i in range(10)}  # 0, 1, 2 ... 9 (가능한 수)
-
-#     # input
-#     N = int(input())  # 이동하려고 하는 채널
-#     M = int(input())  # 고장난 버튼의 개수
-#     if M != 0:
-#         enable -= set(map(str, input().split()))  # 고장난 버튼 리스트 제거
-
-#     # case1 (100번에서 +, - 로만 움직이는 경우)
-#     min_cnt = abs(100 - N)
-
-#     # case2 (1,000,000 채널까지 브루트 포스 진행)
-#     # 500,000 채널까지 존재하기 때문에 500,000 보다 크면서 모든 숫자의 경우를 거치는 1,000,000까지를 범위로 잡음
-#     for num in range(1000001):
-#         num = str(num)
-#         for j in range(len(num)):
-#             if num[j] not in enable:
-#                 break
-#             elif j == len(num) - 1:
-#                 min_cnt = min(min_cnt, abs(N - int(num)) + len(str(num)))
-#     print(min_cnt)
