3d7c463e8881403f522b55eb4756f475af7fc2a6
SeoYeseul <sok98@naver.com>
2021-08-24 15:11:28 +0900

    Refactor: 위클리챌린지 4주차

--- 0823/위클리챌린지4주차.py
+++ 0823/위클리챌린지4주차.py
@@ -1,14 +1,13 @@
 def solution(table, languages, preference):
     answer = []
-    # new_table = [list(t.split()) for t in table]
     new_table = sorted([list(t.split()) for t in table], key=lambda x : x[0])
     job = {n: new_table[n][0] for n in range(len(new_table))}
 
     for j in range(len(job)):
         total = 0
-        for i in range(len(languages)):
-            if languages[i] in new_table[j]:
-                total += (6 - new_table[j].index(languages[i])) * preference[i]
+        for lang, pref in zip(languages, preference):
+            if lang in new_table[j]:
+                total += (6 - new_table[j].index(lang)) * pref
         answer.append(total)
 
     return job[answer.index(max(answer))]
