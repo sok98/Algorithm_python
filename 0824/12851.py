f5af3ece704a0dfb120136809b169c52904cfd70
SeoYeseul <sok98@naver.com>
2021-08-25 15:40:19 +0900

    ADD 백준 12851 숨바꼭질 2

--- 0824/12851.py
+++ 0824/12851.py
@@ -9,24 +9,23 @@ if K == N:      # 둘이 같은 위치에 있을 때
 elif K < N :    # 동생이 뒤에 있을 때
     print(N-K)
     print(1)
-else:
-    time = 1
-    queue = [K-1, K+1]
-    if K % 2 == 0:
-        queue.append(K//2)
-   
-    while queue:
-        tmp = []
-        time += 1
-        for k in queue:
-            if k % 2 == 0:
-                tmp.append(k//2)
-            tmp.append(k-1)
-            tmp.append(k+1)
-        
-        if tmp.count(N) > 0:
-            print(time)
-            print(tmp.count(N))
-            break
 
-        queue = list(set(tmp))
+else:
+    visited = [0 for _ in range(100001)]
+    queue = [N]
+    time = 0
+
+    while queue:
+        time += 1
+        next_queue = []
+        for n in queue:
+            if (n >= 0 and n <= 100000) and (visited[n] == 0 or visited[n] == time):
+                next_queue.extend([n*2, n-1, n+1])
+                visited[n] = time
+
+        if next_queue.count(K) > 0:
+            print(time) 
+            print(next_queue.count(K))
+            break
+        
+        queue = next_queue
