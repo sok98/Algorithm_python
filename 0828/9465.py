0daa3ba07353504441e636304a889a235daf1381
SeoYeseul <sok98@naver.com>
2021-08-28 21:57:58 +0900

    Correct 백준 9465 스티커 : 변수명 변경

--- 0828/9465.py
+++ 0828/9465.py
@@ -5,17 +5,17 @@ T = int(input())
 
 for _ in range(T):
     n = int(input())
-    stikers = [list(map(int, input().split())) for _ in range(2)]
+    stickers = [list(map(int, input().split())) for _ in range(2)]
 
     if n == 1:
-        print(max(stikers[0][0], stikers[1][0]))
+        print(max(stickers[0][0], stickers[1][0]))
 
     else:
-        stikers[0][1] += stikers[1][0]
-        stikers[1][1] += stikers[0][0]
+        stickers[0][1] += stickers[1][0]
+        stickers[1][1] += stickers[0][0]
         for i in range(2, n):
-            stikers[0][i] += max(stikers[1][i-1], stikers[1][i-2])
-            stikers[1][i] += max(stikers[0][i-1], stikers[0][i-2])
+            stickers[0][i] += max(stickers[1][i-1], stickers[1][i-2])
+            stickers[1][i] += max(stickers[0][i-1], stickers[0][i-2])
 
-        print(max(stikers[0][n-1], stikers[1][n-1]))
+        print(max(stickers[0][n-1], stickers[1][n-1]))
         
