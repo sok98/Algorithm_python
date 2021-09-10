e2ad96a50877075ea75877de764c06aa8a1ab088
SeoYeseul <sok98@naver.com>
2021-09-10 15:42:27 +0900

    ADD 프로그래머스 수식 최대화

---
+++ 9월/0910/수식 최대화.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+def solution(expression):
+    answer = 0
+
+    expression = expression.replace("-", " - ").replace("+", " + ").replace("*", " * ").split()
+    operator = [i for i in ['*', '+', '-'] if i in expression]
+    
+    for op in permutations(operator, len(operator)):
+        calcul = expression.copy()
+        for o in op:
+            while o in calcul:
+                i = calcul.index(o)
+                if o == '*':
+                    calcul[i] = int(calcul[i-1]) * int(calcul[i+1])
+                    del calcul[i-1]
+                    del calcul[i]
+                elif o == '+':
+                    calcul[i] = int(calcul[i-1]) + int(calcul[i+1])
+                    del calcul[i-1]
+                    del calcul[i]
+                elif o == '-':
+                    calcul[i] = int(calcul[i-1]) - int(calcul[i+1])
+                    del calcul[i-1]
+                    del calcul[i]
+        answer = max(answer, (abs(calcul[0])))
+
+    return answer
+
+
+print(solution("50*6-3*2"))
