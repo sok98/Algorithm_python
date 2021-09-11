4e3973b2d9cc3e37120108f5f392daf6c78dcaa1
SeoYeseul <sok98@naver.com>
2021-09-11 09:05:32 +0900

    Refactor: eval 사용

--- 9월/0910/수식 최대화.py
+++ 9월/0910/수식 최대화.py
@@ -2,7 +2,6 @@ from itertools import permutations
 
 def solution(expression):
     answer = 0
-
     expression = expression.replace("-", " - ").replace("+", " + ").replace("*", " * ").split()
     operator = [i for i in ['*', '+', '-'] if i in expression]
     
@@ -11,18 +10,7 @@ def solution(expression):
         for o in op:
             while o in calcul:
                 i = calcul.index(o)
-                if o == '*':
-                    calcul[i] = int(calcul[i-1]) * int(calcul[i+1])
-                    del calcul[i-1]
-                    del calcul[i]
-                elif o == '+':
-                    calcul[i] = int(calcul[i-1]) + int(calcul[i+1])
-                    del calcul[i-1]
-                    del calcul[i]
-                elif o == '-':
-                    calcul[i] = int(calcul[i-1]) - int(calcul[i+1])
-                    del calcul[i-1]
-                    del calcul[i]
-        answer = max(answer, (abs(calcul[0])))
+                calcul = calcul[:i-1] + [str(eval(''.join(calcul[i-1:i+2])))] + calcul[i+2:]
+        answer = max(answer, (abs(int(calcul[0]))))
 
     return answer
